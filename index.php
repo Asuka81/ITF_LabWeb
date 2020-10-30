be9bf987b3ff43d65cecba0bf635570f5c4fb507
Asuka81 <71114413+Asuka81@users.noreply.github.com>
2020-10-30 12:13:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo "<h2>Sawasdeekub than samachick!</h2>";
-echo "Chomromit!<br>";
-?>
+<html>
+    <head>
+        <title>Wonsapat Asawongsanon O 63070153</title>
+    </head>
+    <body>
+        <h1></h1>
+        <p>Peter Piper picked a peck of pickled peppers
+           A peck of pickled peppers Peter Piper picked
+           If Peter Piper picked a peck of pickled peppers
+            Where’s the peck of pickled peppers Peter Piper picked?</p>
+        <img src="https://i.ytimg.com/vi/fz7Jxg7Hbkg/maxresdefault.jpg>
+        <img src="https://i.ytimg.com/vi/OmrM9yJrXv0/hqdefault.jpg>
+        <br>
+        <a href="https://www.facebook.com">This is a Link</a>
+        <a href="https://www.youtube.com">Youtube Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+        
+    </body>
+</html>
