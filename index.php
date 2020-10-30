598a0f9bdf19e69242a94e6258b536b84eec8c66
Asuka81 <71114413+Asuka81@users.noreply.github.com>
2020-10-30 12:15:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,18 +8,11 @@
            A peck of pickled peppers Peter Piper picked
            If Peter Piper picked a peck of pickled peppers
             Where’s the peck of pickled peppers Peter Piper picked?</p>
-        <img src="https://i.ytimg.com/vi/fz7Jxg7Hbkg/maxresdefault.jpg>
-        <img src="https://i.ytimg.com/vi/OmrM9yJrXv0/hqdefault.jpg>
+        <img src="https://i.ytimg.com/vi/fz7Jxg7Hbkg/maxresdefault.jpg">
+        <img src="https://i.ytimg.com/vi/OmrM9yJrXv0/hqdefault.jpg">
         <br>
         <a href="https://www.facebook.com">This is a Link</a>
         <a href="https://www.youtube.com">Youtube Link</a>
-
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
         
     </body>
 </html>
