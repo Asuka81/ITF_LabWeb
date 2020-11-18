8ade365a0e15e83e34dbe50c17cec2fbb79f5db4
Asuka81 <71114413+Asuka81@users.noreply.github.com>
2020-11-18 23:13:27 +0700

    Update del.php

--- del.php
+++ del.php
@@ -2,7 +2,7 @@
 $id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'it63070153.mysql.database.azure.com', 'it63070153@it63070153', 'Unknown1234dd', 'ITFLAB', 3306);
-$sql="DELETE FROM guestbook WHERE ID='$id'";
+$sql="DELETE FROM Guestbook WHERE ID='$id'";
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
