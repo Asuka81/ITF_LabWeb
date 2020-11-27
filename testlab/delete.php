9e5f8205e62020f542b4cc8d3ba905f5c14848a0
Asuka81 <71114413+Asuka81@users.noreply.github.com>
2020-11-27 12:33:31 +0700

    Update delete.php

--- testlab/delete.php
+++ testlab/delete.php
@@ -3,8 +3,8 @@ require('connect.php');
 $id=$_GET['id'];
 $sql="DELETE FROM datainsert WHERE id='$id'";
 if (mysqli_query($conn, $sql)) {
-    echo "insert data successfully";
-    echo "<a href='index.php'>back to home page</a>";
+    echo "ลบข้อมูลสำเร็จ";
+    echo "<a href='index.php'>กลับไปหน้าแรก</a>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
